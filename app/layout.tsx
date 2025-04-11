b301dbfbe53881b30883e2e6c702bd7f30aa2bfe
Daniel Buitrago <danielb.alzate1@gmail.com>
2025-04-11 11:59:08 -0500

    Add window.svg icon to public directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${monatSans.className} antialiased`}
+        className={`${monatSans.className} antialiased pattern`}
       >
         {children}
       </body>
